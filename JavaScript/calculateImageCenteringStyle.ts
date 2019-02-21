de3d3e69889d49f0acb8a5f4811924d932641517
mmmpa <mmmpa.mmmpa@gmail.com>
2019-02-21 11:39:25 +0900

    Update calculateImageCenteringStyle.ts

--- JavaScript/calculateImageCenteringStyle.ts
+++ JavaScript/calculateImageCenteringStyle.ts
@@ -1,11 +1,11 @@
 // expect an image for a circle container
-export default function calculateImageCenteringStyle (image: HTMLImageElement, container: HTMLElement) {
+type FitStyle = { [key: string]: string }
+export default function calculateImageCenteringStyle (image: HTMLImageElement, container: HTMLElement): FitStyle {
   const { naturalWidth, naturalHeight } = image;
   const { clientWidth: size } = container;
 
-  return naturalHeight === naturalWidth
-    ? {}
-    : naturalHeight > naturalWidth
-      ? { width: `${size}px`, height: 'auto', marginTop: `${(naturalWidth - naturalHeight) * size / naturalWidth / 2}px` }
-      : { width: 'auto', height: `${size}px`, marginLeft: `${(naturalHeight - naturalWidth) * size / naturalHeight / 2}px` };
+  return naturalHeight > naturalWidth
+    ? { width: `${size}px`, height: 'auto', marginTop: `${(naturalWidth - naturalHeight) * size / naturalWidth / 2}px` }
+    : { width: 'auto', height: `${size}px`, marginLeft: `${(naturalHeight - naturalWidth) * size / naturalHeight / 2}px` };
 }
+
